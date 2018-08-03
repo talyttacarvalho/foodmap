897b5b59996265f0092ee8335dafc40f6e004697
Talytta Carvalho <talytta110@hotmail.com>
2018-08-03 16:25:10 -0300

    mapa adicionado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
 $(document).ready(function () {
-  $(".bg").delay("5000").fadeToggle("slow");
+  $("#screen-one").delay("3000").fadeToggle("slow");
+  $("#screen-two").delay("3000").fadeIn("slow");
 });
+
+function initMap() {
+
+  var uluru = {lat: -25.344, lng: 131.036};
+
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+
+// var mapa;
+// function initMap() {
+//  mapa = new google.maps.Map(document.getElementById('map'), {
+//    center: {lat: -23.5576364,
+//             lng: -46.6628473},
+//    zoom: 14
+//  });
+
+// //Pegar dados Json
+// var features = restaurantes.map(function(r){
+// return {
+//    position: new google.maps.LatLng(r.latitude, r.longitude),
+//    type: 'info'
+//   }
+// });
+// // Criar os pontos.
+// features.forEach(function(feature) {
+//   var marker = new google.maps.Marker({
+//     position: feature.position,
+//     // icon: icons[feature.type],
+//     map: mapa
+//   });
+// });
+// }
